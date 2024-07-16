a80093e8774df9e35cafad9e608023aa44ff8b85
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-16 21:00:58 +0900

    Create JwtFilter.java

---
+++ src/main/java/com.apple.shop/member/JwtFilter.java
@@ -0,0 +1,20 @@
+package com.apple.shop.member;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, 
+      HttpServletResponse response, 
+      FilterChain filterChain
+  ) throws ServletException, IOException {
+    
+    System.out.println("필터실행됨");
+    //요청들어올때마다 실행할코드~~
+    filterChain.doFilter(request, response);
+  }
+
+}
