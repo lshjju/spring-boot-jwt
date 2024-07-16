a2822a72663cfa447baa4d2c52da227f3eef0351
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-16 21:13:55 +0900

    Update JwtFilter.java

--- src/main/java/com.apple.shop/member/JwtFilter.java
+++ src/main/java/com.apple.shop/member/JwtFilter.java
@@ -11,9 +11,11 @@ public class JwtFilter extends OncePerRequestFilter {
       HttpServletResponse response, 
       FilterChain filterChain
   ) throws ServletException, IOException {
-    
-    System.out.println("필터실행됨");
-    //요청들어올때마다 실행할코드~~
+
+    // Cookie[] cookies = request.getCookies();
+    // System.out.println(cookies[0].getNam());
+    // System.out.println("필터실행됨");
+    // 요청들어올때마다 실행할코드~~
     filterChain.doFilter(request, response);
   }
 
