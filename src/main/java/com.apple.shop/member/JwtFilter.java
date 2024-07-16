c2879580b84bcd9b55b6aeac176135f46fe8f187
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-16 22:35:34 +0900

    Update JwtFilter.java

--- src/main/java/com.apple.shop/member/JwtFilter.java
+++ src/main/java/com.apple.shop/member/JwtFilter.java
@@ -12,8 +12,46 @@ public class JwtFilter extends OncePerRequestFilter {
       FilterChain filterChain
   ) throws ServletException, IOException {
 
+    Cookie[] cookies = request.getCookies();
+    if (cookies == null){
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    var jwtCookie = "";
+    for (int i = 0; i < cookies.length; i++){
+       if (cookies[i].getName().equals("jwt")){
+         jwtCookie = cookies[i].getValue();
+     }
+  }
+
+    Claims claim;
+    try {
+      claim = JwtUtil.extractToken(jwtCookie);
+    } catch (Exception e){
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    var arr = claim.get("authorities").toString().split(",");
+    var authorities = Arrays.stream(arr).map(a -> new SimpleGrantedAuthority(a)).toList();
+    
+
+    var customUser = new CustomUser(claim.get("username").toString(), "none", authorities );
+    customUser.displayName = claim.get("displayName").toString();
+
+    var authToken = new UsernamePasswordAuthenticationToken(
+      customUser, ""
+    );
+    authToken.setDetails(new webAuthenticationDetailsSource()
+                        .buildDetails(request)
+                        );
+    SecurityContextHolder.getContext().setAuthentication(authToken);
+    
+      
+    //System.out.println(jwtCookie);
     // Cookie[] cookies = request.getCookies();
-    // System.out.println(cookies[0].getNam());
+    // System.out.println(cookies[0].getName());
     // System.out.println("필터실행됨");
     // 요청들어올때마다 실행할코드~~
     filterChain.doFilter(request, response);
